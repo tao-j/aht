2d829beffc980c0304f96664495afed321128cba
Tao J <tao-j@outlook.com>
2021-06-02 14:21:46 -0400

    mod: invoke experiments in parallel

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from aht import UnevenUCBActiveRank, TwoStageSimultaneousActiveRank, TwoStageSeparateRank
 import random
 import numpy as np
+import os
+import sys
+import subprocess
 
-def gamma_sweep(algonum, repeat, eps=0.1, delta=0.1, N=10, gg=5.0):
+
+def run(algonum, repeat, eps=0.1, delta=0.1, N=10, M=9, gg=5.0, gb=0.5):
     random.seed(123)
     np.random.seed(123)
 
@@ -14,105 +18,88 @@ def gamma_sweep(algonum, repeat, eps=0.1, delta=0.1, N=10, gg=5.0):
         thetas.append(li + (ri - li) * (np.random.random()))
         # thetas.append(np.power(1.2 / 0.8, i))
 
-    M = 9
-    # for gb in [0.25, 1., 2.5]:
-    #     for gg in [2.5, 5, 10]:
-    # for gb in [2.5]:
-    #     for gg in [2.5]:
-    for gb in [0.5]:
-        for gg in [gg]:
-            gamma = [gg] * (M // 3) + [gb] * (M // 3 * 2)
-            gamma += [gb] * (M - len(gamma))
-            # s = np.linspace(1 / n, 1, n)
-            s = np.log(thetas[:N])
-            tts = []
+    gamma = [gg] * (M // 3) + [gb] * (M // 3 * 2)
+    gamma += [gb] * (M - len(gamma))
+    # s = np.linspace(1 / n, 1, n)
+    s = np.log(thetas[:N])
+    tts = []
 
-            for _ in range(repeat):
-                s_idx = list(range(0, len(s)))
-                random.shuffle(s_idx)
-                s = s[s_idx]
-                # np.random.shuffle(s)
+    for _ in range(repeat):
+        s_idx = list(range(0, len(s)))
+        random.shuffle(s_idx)
+        s = s[s_idx]
+        # np.random.shuffle(s)
 
-                # algo = TwoStageSimultaneousActiveRank(N, M, delta, s, gamma)
-                if algonum == 3:
-                    algo = UnevenUCBActiveRank(N, 1, delta, s, [gg], active=False)
-                elif algonum == 2:
-                    algo = TwoStageSeparateRank(N, M, delta, s, gamma)
-                else:
-                    algo = UnevenUCBActiveRank(N, M, delta, s, gamma, active=algonum)
-                rank_sample_complexity, arg_list = algo.rank()
-                tts.append(rank_sample_complexity)
-                a_ms = list(s[arg_list])
-                a_sorted = sorted(s)
+        # algo = TwoStageSimultaneousActiveRank(N, M, delta, s, gamma)
+        if algonum == 3:
+            # oracle
+            algo = UnevenUCBActiveRank(N, 1, delta, s, [gg], active=False)
+        elif algonum == 2:
+            # two stage
+            algo = TwoStageSeparateRank(N, M, delta, s, gamma)
+        elif algonum == 1:
+            # act
+            algo = UnevenUCBActiveRank(N, M, delta, s, gamma, active=True)
+        elif algonum == 0:
+            # non-act
+            algo = UnevenUCBActiveRank(N, M, delta, s, gamma, active=False)
+        rank_sample_complexity, arg_list = algo.rank()
+        tts.append(rank_sample_complexity)
+        a_ms = list(s[arg_list])
+        a_sorted = sorted(s)
 
-                assert (a_ms == a_sorted)
-                # print("selected users", algo.cU)
-            return int(np.average(tts)), int(np.std(tts))
+        assert (a_ms == a_sorted)
+        # print("selected users", algo.cU)
+    return int(np.average(tts)), int(np.std(tts))
 
 
 if __name__ == "__main__":
-    repeat = 100
+    repeat = 1
     delta = 0.2
     # for delta in np.arange(0.05, 1, 0.05):
-    test_range = range(10, 11, 10)
-    i = 1
-    for gg in [1.0]:
-        print(f"{gg} ----")
-        ox = []
-        oy = []
-        oz = []
-        oo = []
-        for n in test_range:
-            print("Items", n)
-            ox.append(gamma_sweep(algonum=0, repeat=repeat, delta=delta, N=n, gg=gg))
-            oy.append(gamma_sweep(algonum=1, repeat=repeat, delta=delta, N=n, gg=gg))
-            oz.append(gamma_sweep(algonum=2, repeat=repeat, delta=delta, N=n, gg=gg))
-            oo.append(gamma_sweep(algonum=3, repeat=repeat, delta=delta, N=n, gg=gg))
+    n_test_range = list(range(10, 11, 10))
+    m_test_range = [9, 18, 36]
+    gg_range = [0.5, 1.0, 2.5]
+    gb_range = [0.25, 0.5, 1.0]
+    # for gb in [0.25, 1., 2.5]:
+    #     for gg in [2.5, 5, 10]:
 
-    # return
-        import os
+    outdir = f"r{repeat}"
+    outdir = "output"
+    if not os.path.isdir(outdir):
+        os.mkdir(outdir)
+    elif os.path.isfile(outdir):
+        exit(1)
 
-        os.chdir("{:.1f}".format(gg))
-        fmt = lambda x: '{},{}'.format(x[0], x[1])
-        def save_output(fname, param):
-            fout = open(f"{fname}.txt", 'w')
-            fout.write('\n'.join(list(map(fmt, param))))
-            fout.close()
-        save_output("aht-non-act", ox)
-        save_output("aht-act", oy)
-        save_output("aht-staged", oz)
-        save_output("aht-oracle", oo)
+    command = sys.argv[1]
+    if command == "manage":
+        for n in n_test_range:
+            for m in m_test_range:
+                for gg in gg_range:
+                    for gb in gb_range:
+                        for algonum in range(0, 4):
+                            fname = f"n{n}-m{m}-gg{gg:.2f}-gb{gb:.2f}-algo{algonum}"
+                            args = ["python", "main.py", "run", fname]
+                            print("invoked", fname, args)
+                            log_name = os.path.join(outdir, f"{fname}.log")
+                            if os.path.isfile(log_name):
+                                # continue # skip when task exists
+                                pass # override existing task
+                            flog = open(log_name, 'w')
+                            with flog:
+                                p = subprocess.Popen(args, stdout=flog, stderr=flog,
+                                                     start_new_session=True)
 
-        import matplotlib.pyplot
-        import matplotlib.pyplot as plt
-        import numpy as np
+    elif command == "run":
+        fname = sys.argv[2]
+        n, m, gg, gb, algonum = sys.argv[2].split('-')
+        n = int(n[1:])
+        m = int(m[1:])
+        gg = float(gg[2:])
+        gb = float(gb[2:])
+        algonum = int(algonum[4:])
 
-        def plot_output(fname, test_range):
-            x = np.array(test_range)
-            y = []
-            std = []
-            fin = open(fname + ".txt")
-            for line in fin.readlines():
-                ax, astd = line.split(',')
-                y.append(int(ax))
-                std.append(int(astd))
-            ax = plt.errorbar(x, y, std, linestyle='-', marker='x')
-            fin.close()
-            return ax
-
-        plt.figure()
-        plot_output("aht-non-act", test_range)
-        plot_output("aht-act", test_range)
-        plot_output("aht-staged", test_range)
-        ax = plot_output("aht-oracle", test_range)
-
-        plt.legend(["Non-Adaptive User Sampling", "Adaptive User Sampling", "Two Stage Ranking", "Oracle"])
-        fmt = matplotlib.pyplot.ScalarFormatter()
-        ax[0].axes.yaxis.set_major_formatter(fmt)
-        plt.xlabel("Number of items to rank")
-        plt.ylabel("Sample Complexity")
-
-        plt.title("$\gamma_A = 0.5, \gamma_B = {:.1f}$".format(gg))
-        plt.savefig(f'nonacac{i}.pdf')
-        os.chdir("../")
-        i = i + 1
+        avg, std = run(algonum, repeat, 0.1, delta, n, m, gg, gb)
+        f = open(os.path.join(outdir, f"{fname}.txt"), 'w')
+        f.write(f"{avg} {std}")
+        f.close()
