cd8246a3648e7e1abfee2c03c5f714d205156d9a
Tao J <tao-j@outlook.com>
2022-05-03 13:19:01 -0400

    mod: fix formatting

--- probesort.py
+++ probesort.py
@@ -4,13 +4,13 @@ from math import ceil, log2, sqrt, log
 
 
 def twopower(x):
-    return 2**ceil(log2(x))
+    return 2 ** ceil(log2(x))
 
 
 def trans_closure(T, e):
-    #update the transitive closure matrix T
-    #in T, T[i][j] = 1 <=> i>j, T[i][j] = -1 <=> i<j, T[i][j] = 0 <=> i>j or i<j not determined
-    #e=[i,j], to update the relation i<j
+    # update the transitive closure matrix T
+    # in T, T[i][j] = 1 <=> i>j, T[i][j] = -1 <=> i<j, T[i][j] = 0 <=> i>j or i<j not determined
+    # e=[i,j], to update the relation i<j
     i, j = e
     if T[i][j] == -1:
         print('contradiction, opposite order')
@@ -65,12 +65,12 @@ def comprob(nums):
 
 
 def SE(Q, delta):
-    #simulate successive elimination
+    # simulate successive elimination
     n = Q[0]
     phat = Q[1] / Q[0]
     if n > 1 and log2(n) == int(log2(n)):
         t = log2(n)
-        alpha_t = sqrt(log((3.14**2 / 3) * (t**2) / delta) / (2 * n))
+        alpha_t = sqrt(log((3.14 ** 2 / 3) * (t ** 2) / delta) / (2 * n))
         if phat - 1 / 2 > alpha_t:
             return 1
         elif phat - 1 / 2 < -alpha_t:
@@ -103,84 +103,93 @@ def findmaxmin(rank, T):
 
 
 # %%
-# randomly generate a permutation and the comparison probability
-n = 20
-sigma = ran_permute(n)
-P = comprob(sigma)
-# start probe sorting
-delta = 0.1
-numcom = [0] * n  #number of comparisons asked involving each item
-rank = [-1] * n
-T = [[0] * n for i in range(n)]  #initialize the transitive closure
-Q = {}  #initialize the queries Q
-for i in range(n):
-    for j in range(i + 1, n):
-        Q[tuple([i, j])] = [0, 0]
+if __name__ == "__main__":
+    # randomly generate a permutation and the comparison probability
+    n = 50
+    sigma = ran_permute(n)
+    P = comprob(sigma)
 
-for t in range(n // 2):
-    print('t=', t)
-    L, M = findmaxmin(rank, T)
-    print(L, M)
-    if len(M) == 1:
-        imin = M.pop()
-    if len(L) == 1:
-        imax = L.pop()
-    while len(L) > 0 or len(M) > 0:
-        change = []
-        for i in range(n):
-            for j in range(i + 1, n):
-                if T[i][j] == 0 and i != j and (i in L.union(M)
-                                                or j in L.union(M)):
-                    x = random.random() < P[i][j]  #ask about i and j once
-                    numcom[i] += 1
-                    numcom[j] += 1
-                    Q[tuple([i, j])][0] += 1
-                    Q[tuple([i, j])][1] += x
-                    if SE(Q[tuple([i, j])], 2 * delta / n**2) == 1:  #means i>j
-                        print(i, '>', j)
-                        change.append([i, j])
-                    elif SE(Q[tuple([i, j])],
-                            2 * delta / n**2) == -1:  #means i<j
-                        print(i, '<', j)
-                        change.append([j, i])
-        for i, j in change:
-            print(i, j)
-            if i in M:
-                M.remove(i)
-            if j in L:
-                L.remove(j)
-            print(L, M)
-            trans_closure(T, [i, j])
-            if len(M) == 1:
-                imin = M.pop()
-            if len(L) == 1:
-                imax = L.pop()
-    rank[t] = imax
-    rank[n - 1 - t] = imin
-    print(rank)
-if n % 2 == 1:
-    rank[n // 2] = int(n * (n - 1) / 2 - sum(rank) - 1)
+    # start probe sorting
+    delta = 0.1
+    numcom = [0] * n  # number of comparisons asked involving each item
+    rank = [-1] * n
+    T = [[0] * n for i in range(n)]  # initialize the transitive closure
+    Q = {}  # initialize the queries Q
+    for i in range(n):
+        for j in range(i + 1, n):
+            Q[tuple([i, j])] = [0, 0]
 
-print('true ranking:', sigma)
-print('output:', rank)
-print('correct?', rank == sigma)
+    samples = 0
+
+    for t in range(n // 2):
+        print('t=', t)
+        L, M = findmaxmin(rank, T)
+        print(L, M)
+        if len(M) == 1:
+            imin = M.pop()
+        if len(L) == 1:
+            imax = L.pop()
+        while len(L) > 0 or len(M) > 0:
+            change = []
+            for i in range(n):
+                for j in range(i + 1, n):
+                    if T[i][j] == 0 and i != j and (i in L.union(M)
+                                                    or j in L.union(M)):
+                        x = random.random() < P[i][j]  # ask about i and j once
+                        samples += 1
+                        numcom[i] += 1
+                        numcom[j] += 1
+                        Q[tuple([i, j])][0] += 1
+                        Q[tuple([i, j])][1] += x
+                        if SE(Q[tuple([i, j])], 2 * delta / n ** 2) == 1:  # means i>j
+                            print(i, '>', j)
+                            change.append([i, j])
+                        elif SE(Q[tuple([i, j])],
+                                2 * delta / n ** 2) == -1:  # means i<j
+                            print(i, '<', j)
+                            change.append([j, i])
+            for i, j in change:
+                print(i, j)
+                if i in M:
+                    M.remove(i)
+                if j in L:
+                    L.remove(j)
+                print(L, M)
+                trans_closure(T, [i, j])
+                if len(M) == 1:
+                    imin = M.pop()
+                if len(L) == 1:
+                    imax = L.pop()
+        rank[t] = imax
+        rank[n - 1 - t] = imin
+        print(rank)
+    if n % 2 == 1:
+        rank[n // 2] = int(n * (n - 1) / 2 - sum(rank) - 1)
+
+    print('true ranking:', sigma)
+    print('output:', rank)
+    print('correct?', rank == sigma)
+
+    # %%
+    estnum = [0] * n
+    for i, x in enumerate(sigma):
+        if i == 0:
+            estnum[x] += 1 / (P[x][sigma[i + 1]] - 1 / 2) ** 2
+        elif i == n - 1:
+            estnum[x] += 1 / (P[sigma[i - 1]][x] - 1 / 2) ** 2
+        else:
+            estnum[x] += 1 / (P[sigma[i - 1]][x] -
+                              1 / 2) ** 2 + 1 / (P[x][sigma[i + 1]] - 1 / 2) ** 2
+    print(numcom)
+    print(estnum)
+
+    from matplotlib import pyplot as plt
+
+    # fig, axs = plt.subplots(2)
+    fig1 = plt.figure()
+    plt.plot(estnum)
+    fig2 = plt.figure()
+    plt.plot(numcom)
+    print("samples", samples)
 
 # %%
-estnum = [0] * n
-for i, x in enumerate(sigma):
-    if i == 0:
-        estnum[x] += 1 / (P[x][sigma[i + 1]] - 1 / 2)**2
-    elif i == n - 1:
-        estnum[x] += 1 / (P[sigma[i - 1]][x] - 1 / 2)**2
-    else:
-        estnum[x] += 1 / (P[sigma[i - 1]][x] -
-                          1 / 2)**2 + 1 / (P[x][sigma[i + 1]] - 1 / 2)**2
-print(numcom)
-print(estnum)
-
-from matplotlib import pyplot as plt
-#fig, axs = plt.subplots(2)
-fig1 = plt.figure()
-plt.plot(estnum)
-fig2 = plt.figure()
-plt.plot(numcom)
