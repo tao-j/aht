ae53d4d621d156022dfecea1ce5e639dc111024c
Tao J <tao-j@outlook.com>
2022-08-24 13:27:28 -0400

    add: lucb sort

--- sort_bordagap.py
+++ sort_bordagap.py
@@ -1,5 +1,64 @@
-class ARalg:
-    def __init__(self, pairwise, kset, epsilon=None):
+# SPDX-License-Identifer: Apache-2.0
+import numpy as np
+
+class LUCBSort:
+    def __init__(self, pairwise, k):
+        self.k = k
+        self.pairwise = pairwise  # instance of pairwise
+        self.pairwise.ctr = 0  # number of random comparisons made
+
+    def random_cmp(self, i):  # compare i to randomly choosen other item
+        j = random.choice(range(self.pairwise.n - 1))
+        if j >= i:
+            j += 1
+        return float(self.pairwise.compare(i, j))
+
+    def alpha(self, Ti):  # confidence interval
+        n = self.pairwise.n
+        beta = log(n / self.delta) + 0.75 * log(log(n / self.delta)) + 1.5 * log(1 + log(Ti / 2))
+        return sqrt(3 / (2 * Ti))
+
+    def rank(self, delta=0.1, numit=6000000):
+        self.delta = delta
+        S = []  # list with entries ( i, T_i, scorehat_i, scorehat_i - alpha_i, scorehat_i + alpha_i, alpha_i)
+        # compare each item once to initialize
+        for i in range(self.pairwise.n):
+            scorehat = self.random_cmp(i)
+            S.append((i, 1, scorehat, scorehat - self.alpha(1), scorehat + self.alpha(1), self.alpha(1)))
+
+        for iit in range(numit):
+            # sort descending by score ('entry' has fields (i, T_i, scorehat_i,...)
+            S = sorted(S, key=lambda entry: entry[2], reverse=True)
+            # min scorehat_i - alpha_i; min over (1),...,(k)
+            d1low = min(S[:self.k], key=lambda entry: entry[3])
+            # max scorehat_i + alpha_i; max over (k+1),...,(n)
+            d2up = max(S[self.k:], key=lambda entry: entry[4])
+
+            if d1low[3] > d2up[4]:  # termination condition
+                break  # terminate
+
+            for it in [d1low, d2up]:  # items to sample in next round
+                Ti = it[1] + 1
+                shat = 1.0 / Ti * ((Ti - 1) * it[2] + self.random_cmp(it[0]))
+                alphai = self.alpha(Ti)
+                S[S.index(it)] = (it[0], Ti, shat, shat - alphai, shat + alphai, alphai)
+        self.S = S
+        estimated_ranking = [s[0] for s in S]
+        self.ranking = [estimated_ranking[:self.k], estimated_ranking[self.k:]]
+
+    def plot_scores(self):  # plot
+        n = len(self.S)
+        scorehat = [self.S[i][2] for i in range(n)]
+        upperest = [self.S[i][3] for i in range(n)]
+        lowerest = [self.S[i][4] for i in range(n)]
+        plt.plot(range(n), scorehat, 'rx', range(n), upperest, 'bx', range(n), lowerest, 'yx')
+        plt.show()
+
+    def evaluate_perfect_recovery(self):  # did it suceed?
+        return set(self.ranking[0]) == set([i for i in range(self.k)])
+
+class BordaGapSort:
+    def __init__(self, model, kset, epsilon=None):
         self.kset = kset  # k_1,..., k_{L-1}, n
         self.pairwise = pairwise  # instance of pairwise
         if epsilon == None:
